d344ee18b438e4407f13bb499d9e895fef0d46c2
Gary Bishop <gary.bishop.unc@gmail.com>
2012-03-04 20:51:15 -0500

    move searchform into the only place it is used.

--- searchform.php
+++
@@ -1,8 +0,0 @@
-<form action="/find/" id="searchform" method="get">
-    <div>
-        <label for="search" class="screen-reader-text">Search for:</label>
-        <input type="search" id="search" name="search" value="" />
-        <input type="hidden" name="page" value="1" />
-        <input type="submit" value="Search" id="searchsubmit" />
-    </div>
-</form>
