407344dd0f73d5fe481852ec569dca6ff6879c9c
Gary Bishop <gary.bishop.unc@gmail.com>
2012-03-21 11:52:04 -0400

    write intermediate state

--- js/write.js
+++ js/write.js
@@ -1,10 +1,13 @@
 define(['jquery',
-        'route'
+        'route',
+        'templates',
+        'jquery.inlineedit'
         ],
-    function($, route) {
+    function($, route, templates) {
 
         var galleryData = {};
         var galleryUrl = '';
+        var $editDialog = null;
 
         function fetchAnotherGallery(step) {
             galleryData.page += step;
@@ -58,12 +61,81 @@ define(['jquery',
                 return false;
             }
             fetchAnotherGallery(0);
-
         }
-        function writeInit(url, query) {
+
+        function addPage(page) {
+            var view = {
+                image: page,
+                caption: page.text
+            };
+            templates.setImageSizes(view.image);
+            var $p = $('<li class="thr-book-page">' + templates.render('bookPage', view) + '</li>');
+            $p.find('a').remove();
+            $('#write-pages').append($p);
+            $('#noPicturesMessage').hide();
+        }
+
+        function initializeBookState(book) {
+            console.log('initBook', book);
+            $('input[title]').val(book.title);
+            $.each(book.pages.slice(1), function (index, page) {
+                addPage(page);
+            });
+        }
+
+        function editPage(e) {
+            var $this = $(this);
+            var $img = $this.find('img');
+            var caption = $this.find('p.thr-caption').html();
+            console.log('eP', $img, caption);
+            var view = {
+                image: {
+                    url: $img.attr('src'),
+                    width: $img.attr('data-width'),
+                    height: $img.attr('data-height')
+                },
+                caption: caption
+            };
+            templates.setImageSizes(view.image);
+            var $content = $(templates.render('bookPage', view));
+            foo = $content;
+            $content.filter('a.thr-credit,a.thr-home-icon,a.thr-settings-icon').hide();
+            if (!$editDialog) {
+                $editDialog = $('<div class="thr-book-page edit-page"></div>').dialog({
+                    width: 'auto',
+                    resizable: false,
+                    modal: true,
+                    draggable: false,
+                    autoOpen: false,
+                    open: function(event, ui) {
+                        $(this).find('.thr-caption').inlineEdit({
+                            buttons: '',
+                            saveOnBlur: true,
+                            control: 'textarea'
+                        });
+
+                    }
+                });
+            }
+            $editDialog.empty().append($content);
+            $editDialog.dialog('open');
+        }
+
+        function writeInit(url, id) {
             // nested require so these are only loaded by users who want to write.
             // TODO: set loading here so user knows to wait
             var $page = this;
+
+            var bookContent = {};
+            if (id) { // if an id was provided, fetch that book for editing
+                bookContent = $.ajax({
+                    url: '/book-as-json/',
+                    data: {
+                        id: id
+                    },
+                    dataType: 'json'
+                });
+            }
 
             // load the jquery-ui css
             $('<link>', {
@@ -73,24 +145,35 @@ define(['jquery',
                 id: 'theme'
             }).appendTo('head');
 
-            require(['jquery-ui', 'jquery.image-gallery'],
+            require(['jquery-ui', 'jquery.image-gallery', 'jquery.ui.touch-punch'],
                 function() {
                     // TODO: Clear loading here
                     // Initialize the accordian look
-                    $('.accordion').each(function(index, item) {
-                        $(item).accordion({
+                    $('.accordion').accordion({
                             collapsible: true,
                             active: false,
-                            clearStyle: true }); });
+                            clearStyle: true });
 
                     // Initialize the Image Gallery widget:
                     $('#gallery').imagegallery({
                         buttons: {
-                            "Add to book": function() { $(this).dialog("close"); },
+                            "Add to book": function() {
+                                var $img = $(this).find('img');
+                                var page = {
+                                    url: $img.attr('src').replace('_s', ''),
+                                    width: $img.attr('width'),
+                                    height: $img.attr('height'),
+                                    text: ''
+                                };
+                                addPage(page);
+                                $('#step2').accordion('activate');
+                            },
                             "Go back to search": function() { $(this).dialog("close"); },
                             "Previous picture": function() { $('#gallery').imagegallery('prev'); },
                             "Next picture": function() { $('#gallery').imagegallery('next'); }
-                        }
+                        },
+                        show: {effect: 'fade', duration: 1 },
+                        hide: {effect: 'fade', duration: 100 }
                     });
                     var $form = $page.find('form');
                     $form.submit(function(e) {
@@ -105,7 +188,6 @@ define(['jquery',
                         return false;
                     });
 
-                    $('.accordion a:first').click();
                     $('.button', '#writing-controls').button().button('disable');
                     $('#gallery-back').click(function() {
                         fetchAnotherGallery(-1);
@@ -115,11 +197,29 @@ define(['jquery',
                         fetchAnotherGallery(+1);
                         return false;
                     });
+                    $('#write-pages').on('click', 'li', editPage);
+                    $('#write-pages').sortable();
 
-                    $('#writing-controls').css('visibility', 'visible');
+                    $.when(bookContent).then(function(book) {
+
+                        if (book.ID) { // editing an existing book
+                            initializeBookState(book);
+                        }
+                        // ie8 insists I made this visible before activating the control
+                        $('#writing-controls').css('visibility', 'visible');
+
+                        if (book.ID) {
+                            // open step 2
+                            $('#step2').accordion('activate');
+                        } else {
+                            // open step 1
+                            $('#step1').accordion('activate');
+                        }
+                    });
+
                 });
         }
 
-        route.add('init', /^\/write\/$/, writeInit);
+        route.add('init', /^\/write\/(?:\?id=(\d+))?$/, writeInit);
 
     });
