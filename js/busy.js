756cbc11344b6d49ccc16649a404767efd1c8ad8
Gary Bishop <gary.bishop.unc@gmail.com>
2012-03-26 15:18:55 -0400

    Handle errors too.

--- js/busy.js
+++ js/busy.js
@@ -20,29 +20,41 @@ define(['jquery', 'templates'], function($, templates) {
             currentXHR.abort();
         }
         currentXHR = jqXHR;
-        $('#busyMessage').css('top', $(window).scrollTop() + $(window).height() / 3 + 'px');
+        $blocker.height($(document).height()).addClass('isBusy').removeClass('isError');
+        $('#busyMessage')
+            .css('top', $(window).scrollTop() + $(window).height() / 3 + 'px');
         $blocker.delay(500).fadeIn(1000);
     }
 
     function done(jqXHR, textStatus) {
-        console.log('done');
+        console.log('done', textStatus);
         if (currentXHR !== jqXHR) {
             console.log('busy: not expecting this XHR to complete, ignoring');
-        } else {
+        } else if (textStatus == 'success') {
             $blocker.stop(true);
             $blocker.hide();
             currentXHR = null;
         }
     }
 
+    function error(jqXHR, textStatus, errorThrown) {
+        if (currentXHR !== jqXHR) {
+            console.log('busy: not expecting error on this XHR, ignoring');
+        } else {
+            $('#errorMessage span').html(textStatus + ': ' + errorThrown);
+            $blocker.removeClass('isBusy').addClass('isError');
+        }
+    }
+
     $.ajaxSetup({
         beforeSend: wait,
-        complete: done
+        complete: done,
+        error: error
     });
 
     $(function() {
         $blocker = $(templates.render('busy')).appendTo('body');
-        $blocker.find('button').on('click', cancel);
+        $blocker.find('button.ajaxCancel').on('click', cancel);
     });
 
     return {
