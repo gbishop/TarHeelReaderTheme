505ecdc54eb6f9a4f8e76c953f943f37d6f1f8ff
Gary Bishop <gary.bishop.unc@gmail.com>
2012-07-17 12:52:14 -0400

    Fixed a bug in reading controls.

--- reading-controls.php
+++ reading-controls.php
@@ -10,7 +10,7 @@ Allow users to configure reading
 
 // get the id if any
 $ID = getParam('id', '', '/[0-9]+/');
-$backto = getGet('backto', '/find/');
+$backto = getParam('backto', '/find/');
 $settingsFormData = setFormFromState($Templates['readingForm']);
 $settingsFormData['action'] = parse_url($backto, PHP_URL_PATH);
 $view = array();
