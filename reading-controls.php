cdb0495c7b309bc2703fcca3ccbc01ce195827b3
Gary Bishop <gary.bishop.unc@gmail.com>
2012-09-04 15:55:46 -0400

    Favorites on reading controls page.

--- reading-controls.php
+++ reading-controls.php
@@ -9,13 +9,18 @@ Allow users to configure reading
 <?php
 
 // get the id if any
-$ID = getParam('id', '', '/[0-9]+/');
+$ID = getParam('id', '', '/^\d+$/');
 $backto = getParam('backto', '/find/');
 $settingsFormData = setFormFromState($Templates['readingForm']);
 $settingsFormData['action'] = parse_url($backto, PHP_URL_PATH);
 $view = array();
 $view['settingsForm'] = template_render('form', $settingsFormData);
 $view['ID'] = $ID;
+$favorites = THR('favorites');
+$favorites = $favorites ? explode(',', $favorites) : array();
+$view['FavCount'] = count($favorites);
+$view['FavAdd'] = !in_array($ID, $favorites);
+$view['backto'] = $backto;
 echo template_render('readingControls', $view);
 ?>
 <?php thr_footer(false, true); ?>
