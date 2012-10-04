9e21da72f2613402747c856761134281e5eea4a4
Gary Bishop <gary.bishop.unc@gmail.com>
2012-10-04 12:04:27 -0400

    Issue #20.
    Apparently in their great wisdom Apple changed
    the rules. After these mods, speech appears to
    work again.

--- js/speech.js
+++ js/speech.js
@@ -12,33 +12,35 @@ define(["jquery", "templates" ], function($, templates) {
         // use html5 audio if it is available and if it supports mp3. I'd rather use ogg but I need mp3 for
         // flash fallback anyway
         // to make it work on the iPad I apparently have to load a legal mp3. Use this one for now.
-        if (typeof(Audio) === 'function') {
+        console.log('initialize audio');
+        if (typeof(Audio) !== 'undefined') {
+            console.log('have Audio function');
             audio = new Audio(SpeechBase + 'site/en-c-whatnow.mp3');
-            
+
            if (audio && audio.canPlayType &&
                 ("no" != audio.canPlayType("audio/mpeg")) &&
                 ("" !== audio.canPlayType("audio/mpeg"))) {
+                console.log('passed audio test');
                 $('.flashplayer').remove();
                 // we appear to have html5 audio so call load which is required on the iPod.
                 audio.load();
                 // now this node is blessed so we can play sound whenever we want.
 
             } else {
+                console.log('failed audio test, using flash');
                 // otherwise signal that we'll be using flash
                 audio = 'flash';
             }
         } else {
+            console.log('defaulting to flash');
             audio = 'flash';
         }
     }
 
     $(function() {
         if (navigator.userAgent.match(/iPad/i) !== null || navigator.platform.match(/iPhone|iPod/) !== null) {
-            // wait for a user interaction to initialize
-            $('body').on('tap.audio', function(e) {
-                // unhook the event
-                $('body').off('tap.audio');
-                // create the audio node
+            // wait for a user interaction to initialize audio because Apple knows best
+            $(document.body).one('mousedown', function(e) {
                 initialize();
             });
         } else { // for anyone else I should be able to simply initialize
@@ -49,7 +51,9 @@ define(["jquery", "templates" ], function($, templates) {
     function play(id, lang, voice, page) {
         voice = voice[0]; // assure we're only using the 1st letter
         console.log('play', id, lang, voice, page);
-        if (!audio || voice === 's' || !hasSpeech[lang]) return;
+        if (!audio || voice === 's' || !hasSpeech[lang]) {
+            return;
+        }
 
         var mp3 = SpeechBase;
         if (id == 'site') {
@@ -61,7 +65,6 @@ define(["jquery", "templates" ], function($, templates) {
 
         if (audio === 'flash') {
             $('.flashplayer').remove();
-            console.log('about to render', mp3);
             var view = { eurl: encodeURI(mp3) };
             $('body').append(templates.render('flash', view));
         } else {
