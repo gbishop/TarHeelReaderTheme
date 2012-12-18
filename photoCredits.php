b00819c8b59532b1ce62bbe9cc368137142f4680
Gary Bishop <gary.bishop.unc@gmail.com>
2012-12-18 11:16:09 -0500

    Issue #37 photo credits page
    Add links to the page for each photo.

---
+++ photoCredits.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Template Name: PhotoCredits
+
+Link back to Flickr to give photographers credit
+*/
+?>
+<?php thr_header(''); ?>
+<!-- photoCredits.php -->
+<?php
+    if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+    <article>
+        <div <?php post_class() ?> >
+
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+
+            <div class="entry-content">
+
+                <?php the_content(); ?>
+<?php
+
+// get the bookid
+$ID = getParam('id', '', '/^\d+$/');
+if ($ID) {
+    $post = get_post($ID);
+    $book = ParseBookPost($post);
+    $view = array();
+    $view['title'] = $book['title'];
+    $view['ID'] = $ID;
+    $photos = array_slice($book['pages'], 1);
+    foreach ($photos as &$photo) {
+        if (strpos($url, '/uploads/') === false) {
+            $parts = explode('/', $photo['url']);
+            $id = explode('_', $parts[count($parts)-1]);
+            $id = $id[0];
+            $photo['infolink'] = "http://flickr.com/photo.gne?id=$id";
+        }
+        setImageSizes($photo);
+    }
+    $view['photos'] = $photos;
+    echo template_render('photoCredits', $view);
+}
+?>
+            </div>
+        </div>
+    </article>
+<?php
+    endwhile; endif; ?>
+<?php thr_footer(false, true); ?>
