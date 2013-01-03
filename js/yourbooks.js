9bd5e31a598d9ecdc76df326573728557c05745a
Cenk Baykal <baykal@live.unc.edu>
2013-01-03 03:09:45 -0500

    quick navigation code; scrolling should be better now

--- js/yourbooks.js
+++ js/yourbooks.js
@@ -5,32 +5,39 @@ define([
     "jquery.scrollIntoView"
 ], function($, route, state) {
 
+     $('body').on('PageRendered', '.your-books-page', function() {
+        var $this = $(this),
+        numDraftBooks = $this.find('.draftBooks').children().length,
+        numPublishedBooks = $this.find('.publishedBooks').children().length;
+        // show the div for quick navigation if the added total of books equals or exceeds 20
+        if((numDraftBooks + numPublishedBooks) >= 20) {
+            $this.find('.quickNavigation')
+                 .slideDown();
+        }
+        console.log('in page rendered');
+    }); // end PageRendered
+
     function hideControls() {
         $('.active-page .controlList li.active').removeClass('active')
-                                                .find("div").slideUp(300);
-
+                                                .find("div")
+                                                .slideUp(300);
     }
-
+    
     // display the controls on click
     $(document).on('click', '.controlList li span', function(ev) {
         var $parentLi = $(this).parent(),
-            $optionsDiv = $parentLi.find("div");
+            $optionsDiv = $parentLi.find("div"),
+            numChildren = $optionsDiv.children();
 
         // hide any that are shown
         hideControls();
-
-        // scroll into view and show the options div
-        $optionsDiv.slideDown(300, "swing", function() {
-            /* disable for now, it mostly works without it
-                $(this).scrollIntoView(200, "swing", function() {
-                var parentList = $parentLi.parent();
-                $optionsDiv.parents("ul").animate({scrollTop: $optionsDiv.position().top - parentList.position().top}, 600);
-            }); */
-        }); // can't chain functions here
-
-
+        
         // signal that it is open so I can validate the state of some buttons
         $parentLi.addClass("active").trigger('activated');
+        // scroll into view and show the options div
+        $optionsDiv.slideDown(300, "swing", function() {
+            $optionsDiv.scrollIntoView();
+        });
     });
 
     // hide the controls on cancel
@@ -54,7 +61,6 @@ define([
                 $buttons = $this.find('.requireFavs');
             $buttons.prop('disabled', favCount == 0);
         });
-
     }
 
     // function to remove the book from the list so we don't need a page refresh to update the lists
@@ -69,7 +75,6 @@ define([
                     success = false;
                 }
             }
-
             if(success) { // remove the list item
                 $div.slideUp(300, "swing", function() {
                     $li.remove();
@@ -78,7 +83,6 @@ define([
                         $bookList.replaceWith("<p>If you had any books saved as drafts they would appear here.</p>");
                     }
                 }); // end slideUp
-
             } else { // something went wrong... just slide up
                 $li.removeClass("active");
                 $div.slideUp(300);
@@ -87,22 +91,6 @@ define([
     }
 
     route.add('init', /^\/your-books\/(\?.*)?$/, initYourBooks);
-
-
-    /* This is what I originally intended, but it is probably safer to check that the ajax call succeeded
-     * and that the deletion was also successful
-     *
-     *
-     * if it is a delete button, update the list accordingly without needing a page refresh
-     $(document).on('click', '.controlList.bookList button[data-action="delete"]', function(event) {
-        var $li = $(this).parents('li'),
-            $div = $li.find('div');
-
-        $div.slideUp(300, function() {
-            $li.remove(); // remove the list item entirely
-        });
-
-    });*/
 
     // handle drafts/books buttons
     $(document).on('click', '.controlList.booksList button[data-action!="cancel"]', function(event) {
@@ -114,8 +102,9 @@ define([
         if(action === 'edit') {
            window.location.pathname = "/write/?id=" + bookID; // simply change URL, this is all we need to do, right?
         } else if(action === 'delete') {
-           $.post('/your-books/', {action: action, id: bookID }, function(data, status) {
-               removeFromList(data, status, $li, $li.find('div')); // remove from list if successful
+           $.post('/your-books/', {action: action + '-draft', id: bookID }, function(data, status) {
+               // removeFromList(data, status, $li, $li.find('div')); // we can use this for deletion without reload
+               window.location.reload(true);  // keep the deletion behavior consistent: refresh page
            }, 'json');
         }
     }); // end click
@@ -146,7 +135,7 @@ define([
         }
         if (args) {
             $.post('/your-books/', args, function(data, txtStatus) {
-                window.location.reload(false);    // do we need this if we can just update the list without a refresh?
+                window.location.reload(false); 
             }, 'json');
         }
     });
