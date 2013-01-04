4e2d6a130f4a575c1b96a5227261245fbd14ac07
Gary Bishop <gary.bishop.unc@gmail.com>
2013-01-04 18:58:23 -0500

    Issue #82 Login page
    Now it is a translated template, the links are styled as buttons,
    and it doesn't break App mode.
    
    The style still needs work but we can do that later.

--- login.php
+++ login.php
@@ -5,54 +5,24 @@ Template Name: LoginForm
 Display the login form
 */
 ?>
-<?php if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+<?php
+$view = array();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $user = wp_signon();
     if ( is_wp_error($user) ) {
-        if ( is_ajax() ) {
-            echo 'No';
-            exit;
-        } else {
-            $msg = 'Login failed';
-        }
+        $view['failed'] = 1;
     } else {
-        if ( is_ajax() ) {
-            echo 'OK';
-            exit;
-        } else {
-            wp_redirect('/');
-            exit;
-        }
+        wp_redirect('/');
     }
 }
-?>
-<?php thr_header(''); ?>
+$view['logged_in'] = is_user_logged_in();
+$view['admin'] = is_admin();
+$view['logoutURL'] = wp_logout_url("/");
+
+thr_header('login-page'); ?>
 <!-- login.php -->
 <?php
-if ( ! is_user_logged_in() ): ?>
-<h2>Login to Tar Heel Reader</h2>
-<p class="message"><?php echo $msg ?></p>
-<form class="loginForm" action="/login/" method="post">
-<p>
-<label>Username<br />
-<input class="thr-input" type="text" name="log" class="input" value="" size="20" tabindex="10" /></label>
-</p>
-<p>
-<label>Password<br />
 
-<input class="thr-input" type="password" name="pwd" class="input" value="" size="20" tabindex="20" /></label>
-</p>
-<p class="forgetmenot"><label><input name="rememberme" type="checkbox" value="forever" tabindex="90" /> Remember Me</label></p>
-<p class="submit">
-<input class="thr-blue-button"type="submit" name="wp-submit" class="button-primary" value="Log In" tabindex="100" />
-<input type="hidden" name="redirect_to" value="/" />
-<input type="hidden" name="testcookie" value="1" />
-</p>
-</form>
-<?php
-    wp_register('<br/>', '<br/>');
-else:
-    wp_loginout( home_url() ); // display log out link
-    wp_register('<br/>', '<br/>');
-endif;
+echo template_render('login', $view);
 
 thr_footer(true, true); ?>
