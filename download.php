033567663770570e43b5670e8a3e12b812e586fe
Gary Bishop <gary.bishop.unc@gmail.com>
2013-01-16 11:39:53 -0500

    Issue #96, EPUB.
    Now you can download a book in EPUB format. They appear to work on the iPad. They
    don't look so good in the Ubuntu e-reader but maybe there is some problem with it?
    
    I have done almost nothing to the style. It will be easy to tweak the stylesheet
    in EPub/page.html if needed.

--- download.php
+++ download.php
@@ -9,16 +9,8 @@ GET: /download/?id=12345&type=ppt  return PowerPoint for a book
 */
 
 /** Error reporting */
-//error_reporting(E_ALL);
+error_reporting(E_ALL);
 
-/** Include path **/
-set_include_path(get_include_path() . PATH_SEPARATOR . '/var/www/tarheelreader/theme/Classes/');
-
-/** PHPPowerPoint */
-include 'PHPPowerPoint.php';
-
-/** PHPPowerPoint_IOFactory */
-include 'PHPPowerPoint/IOFactory.php';
 
 if($_SERVER['REQUEST_METHOD'] == 'GET') {
     // get the parameters
@@ -44,20 +36,15 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
             die();
         }
     }
-    $type = getParam('type', '', '/pptx/');
+    $type = getParam('type', '', '/pptx|epub/');
     $book = ParseBookPost($post);
 
-    $path = tempnam('/tmp', 'pptx');
+    BuG("type=$type");
     if ($type == 'pptx') {
-        CreatePPTXFromBook($path, $book);
+        CreatePPTXFromBook($book);
+    } elseif ($type == 'epub') {
+        CreateEPubFromBook($book);
     }
-    $fp = fopen($path, 'rb');
-    header('Content-Type: application/vnd.openxmlformats-officedocument.presentationml.presentation');
-    header('Content-Disposition: attachment; filename="' . $book['slug'] . '.pptx"');
-    header('Content-Size: ' . filesize($path));
-    fpassthru($fp);
-    fclose($fp);
-    unlink($path);
     exit;
 }
 
@@ -66,7 +53,57 @@ function imgPath($url) {
     return $result;
 }
 
-function CreatePPTXFromBook($path, $book) {
+function CreateEPubFromBook($book) {
+    global $mustache;
+
+    set_include_path(get_include_path() . PATH_SEPARATOR . ABSPATH .
+        'theme/EPub');
+    include_once("EPub.php");
+    $template = file_get_contents(ABSPATH . 'theme/EPub/page.html');
+    $epub = new EPub();
+    $epub->setTitle($book['title']);
+    $epub->setIdentifier($book['link'], EPub::IDENTIFIER_URI);
+    $epub->setLanguage($book['language']);
+    $epub->setDescription('A Downloaded Tar Heel Reader Book');
+    $epub->setAuthor($book['author'], $book['author']);
+    $epub->setPublisher('Tar Heel Reader', 'http://tarheelreader.org');
+    $epub->setRights("Copyright Tar Heel Reader. May be freely used for education. May not be sold.");
+    $epub->setSourceURL($book['link']);
+
+    $pages = $book['pages'];
+    $epub->setCoverImage($pages[1]['url']);
+
+    $N = count($pages);
+    for($i=0; $i<$N; $i++) {
+        $page = $pages[$i];
+        $url = $pages[max(1, $i)]['url'];
+        $view = array();
+        $view['title'] = $book['title'];
+        $view['author'] = $book['author'];
+        $view['url'] = $url;
+        $view['text'] = $page['text'];
+        $view['titlepage'] = $i == 0;
+        $content = $mustache->render($template, $view);
+        $epub->addChapter('page' . $i, 'page' . $i . '.html', $content,
+            false, EPub::EXTERNAL_REF_ADD, ABSPATH);
+    }
+    $epub->finalize();
+    $zipData = $epub->sendBook($book['slug']);
+}
+
+function CreatePPTXFromBook($book) {
+    /** Include path **/
+    set_include_path(get_include_path() . PATH_SEPARATOR . ABSPATH .
+        'theme/Classes/');
+
+    /** PHPPowerPoint */
+    include 'PHPPowerPoint.php';
+
+    /** PHPPowerPoint_IOFactory */
+    include 'PHPPowerPoint/IOFactory.php';
+
+    $path = tempnam('/tmp', 'pptx');
+
     $objPHPPowerPoint = new PHPPowerPoint();
 
     // Set properties
@@ -149,5 +186,13 @@ function CreatePPTXFromBook($path, $book) {
     // Save PowerPoint 2007 file
     $objWriter = PHPPowerPoint_IOFactory::createWriter($objPHPPowerPoint, 'PowerPoint2007');
     $objWriter->save($path);
+
+    $fp = fopen($path, 'rb');
+    header('Content-Type: application/vnd.openxmlformats-officedocument.presentationml.presentation');
+    header('Content-Disposition: attachment; filename="' . $book['slug'] . '.pptx"');
+    header('Content-Size: ' . filesize($path));
+    fpassthru($fp);
+    fclose($fp);
+    unlink($path);
 }
 
