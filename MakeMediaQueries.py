331eb315935871dcd2b73b93b61c2c6001a247a4
Cenk Baykal <baykal@live.unc.edu>
2013-01-18 23:12:20 -0500

    extend the lowest font-size to 9px

--- MakeMediaQueries.py
+++ MakeMediaQueries.py
@@ -13,8 +13,8 @@ mqP = '''@media only screen and (min-height: %dpx) and (min-width: %dpx) and (ma
     }
 }'''
 
-for i in range(11,33):
+for i in range(10,33):
     print mqL % (i*37.5, i*48, i-1)
 
-for i in range(11,33):
+for i in range(10,33):
     print mqP % (i*64, i*48, i*64, i-1, 48, 48)
