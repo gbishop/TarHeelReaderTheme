ef4d274d4710a2d9ab01bb4dfc83e7aa0996b7d5
Gary Bishop <gary.bishop.unc@gmail.com>
2013-01-20 15:49:26 -0500

    Get the scaling right by going to range queries.

--- MakeMediaQueries.py
+++ MakeMediaQueries.py
@@ -13,21 +13,28 @@ If the aspect ratio is less than 4/3 there should be two cases:
 '''
 
 
-mqL = '''@media only screen and (min-aspect-ratio: 4/3) and (min-height: %dpx) {
+mqL = '''@media only screen and (min-aspect-ratio: 4/3) and (min-height: %dpx) and (max-height: %dpx) {
     body {
         font-size: %.2fpx;
     }
 }'''
-mqP = '''@media only screen and (max-aspect-ratio: 4/3) and (min-width: %dpx) {
+mqP = '''@media only screen and (max-aspect-ratio: 4/3) and (min-width: %dpx) and (max-width: %dpx) {
     body {
         font-size: %.2fpx;
     }
 }'''
 
-for d in [360, 400, 440, 480, 540, 640, 768, 800, 960, 1024, 1280]:
-    print mqL % (d, float(d) / 36)
-    if d <= 640:
-        print mqP % (d, float(d) / 36)
+sizes = [320, 360, 400, 420, 440, 460, 480, 512, 540, 600, 624, 640, 720, 768, 800, 960, 1024, 1080, 1280, 1600, 1920]
+
+for i in range(0, len(sizes) - 1):
+    f = sizes[i]
+    t = sizes[i + 1]
+    print mqL % (f, t - 1, float(f) / 36)
+    if t <= 624:
+        print mqP % (f, t - 1, float(f) / 38)
     else:
-        print mqP % (d, float(d) / 48)
+        print mqP % (f, t - 1, float(f) / 50)
 
+# write a final pair for huge displays
+print '@media only screen and (min-aspect-ratio: 4/3) and (min-height: %dpx) { body { font-size: %.2fpx; } }' % (sizes[-1], sizes[-1] / 36)
+print '@media only screen and (max-aspect-ratio: 4/3) and (min-width: %dpx) { body { font-size: %.2fpx; } }' % (sizes[-1], sizes[-1] / 48)
