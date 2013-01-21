32a4a8f64936c32383fd3bc4f0e59f52dd682da1
Gary Bishop <gary.bishop.unc@gmail.com>
2013-01-21 12:21:46 -0500

    Issue #135 remove comments link

--- meta.php
+++ meta.php
@@ -3,5 +3,4 @@
 	<span class="byline author vcard">
 		<i>by</i> <span class="fn"><?php the_author() ?></span>
 	</span>
-	<?php comments_popup_link('No Comments', '1 Comment', '% Comments', 'comments-link', ''); ?>
 </footer>
