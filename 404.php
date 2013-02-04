ce5587a8eed5582a173feb9725756a799226b85b
Gary Bishop <gary.bishop.unc@gmail.com>
2013-02-03 21:31:42 -0500

    Log an event on 404's

--- 404.php
+++ 404.php
@@ -6,7 +6,9 @@
     }
 ?>
 <?php
-thr_header('navigation');
+thr_header('navigation'); ?>
+<script>logEvent('Error', '404', 'page: ' + document.location.pathname + document.location.search + ' ref: ' + document.referrer);</script>
+<?php
 echo template_render('navigation', array('notfound'=>true));
 thr_footer();
 ?>
