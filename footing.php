4e7144cb71c98af4ac2cc037e1c52bf4017029fb
Gary Bishop <gary.bishop.unc@gmail.com>
2013-02-03 21:01:36 -0500

    clean up footing.

--- footing.php
+++
@@ -1,9 +0,0 @@
-        <!-- footing.php -->
-        <footer>
-            <div class="footer clear">
-                <!--
-                <?php bloginfo('name'); ?> is proudly powered by <a href="http://wordpress.org/">WordPress</a> <a href="<?php bloginfo('rss2_url'); ?>">RSS Feed</a>. -->
-                <?php if(current_user_can('level_10')) { echo get_num_queries(),' queries in ', timer_stop(0), ' seconds'; } ?>
-            </div>
-        </footer>
-
