7849a8c7361609ab14e3a63998e4468a13e3cc2a
Gary Bishop <gary.bishop.unc@gmail.com>
2013-02-03 20:19:02 -0500

    Redirect on old style search url's
    I'm trying to pick up common landing pages.

--- 404.php
+++ 404.php
@@ -1,4 +1,11 @@
 <?php
+    $s = getParam('s', '');
+    if ($s) {
+        header("Location: /find/?search=$s");
+        die();
+    }
+?>
+<?php
 thr_header('navigation');
 echo template_render('navigation', array('notfound'=>true));
 thr_footer(false, true);
