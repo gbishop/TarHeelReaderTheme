2186b4f44c4a1815285455090597e855d9283378
Gary Bishop <gary.bishop.unc@gmail.com>
2013-02-06 16:37:05 -0500

    error not Error.

--- 404.php
+++ 404.php
@@ -7,7 +7,7 @@
 ?>
 <?php
 thr_header('navigation'); ?>
-<script>logEvent('Error', '404', 'page: ' + document.location.pathname + document.location.search + ' ref: ' + document.referrer);</script>
+<script>logEvent('error', '404', 'page: ' + document.location.pathname + document.location.search + ' ref: ' + document.referrer);</script>
 <?php
 echo template_render('navigation', array('notfound'=>true));
 thr_footer();
