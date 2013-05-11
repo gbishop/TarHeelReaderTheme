4dc20850ed03e77256feeb41ac59f73f7a6c77b4
Gary Bishop <gary.bishop.unc@gmail.com>
2013-05-11 19:54:43 -0400

    Disable Google Analytics for Safari previews.

--- header.php
+++ header.php
@@ -43,14 +43,17 @@
     <script>
         // Google Analytics
         var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-6128682-1']);
-        _gaq.push(['_trackPageview']);
+        <?php if ($_SERVER['HTTP_X_PURPOSE'] != 'preview') : /* don't count Safari preview visits */ ?>
 
-        (function() {
-          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();
+            _gaq.push(['_setAccount', 'UA-6128682-1']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+              var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+              ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+              var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+        <?php endif; ?>
 
         <?php if (THR('debug') == 1): ?>
             function logMessage(msg) {
