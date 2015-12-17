0e61af9649a26cfc206702171590c5d42fe8690b
Gary Bishop <gary.bishop.unc@gmail.com>
2015-12-17 14:55:16 -0500

    cleanup footer

--- footer.php
+++ footer.php
@@ -3,19 +3,5 @@
 
     <?php wp_footer(); ?>
 
-    <?php if (!THR('classic')) : ?>
-        <![if gt IE 7]>
-<!--
-        <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
-        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
-        <script>var require = { waitSeconds: 200 };</script>
-        <script data-main="/theme/js/main" src="/theme/js/require.js"></script>
--->
-        <![endif]>
-    <?php else : ?>
-        <script>
-            logEvent('classicmode', 'on', 'now');
-        </script>
-    <?php endif ?>
 </body>
 </html>
