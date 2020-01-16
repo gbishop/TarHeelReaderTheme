6791e38b21c813d4446181062b3eddb2d23b2533
Gary Bishop <gary.bishop.unc@gmail.com>
2020-01-16 10:41:57 -0500

    Make favorites use the shared reader.

--- favorites.php
+++ favorites.php
@@ -4,10 +4,9 @@ Template Name: Favorites
 
 GET: Return a list of books that match the query
 */
-?>
-<?php
 // handle converting old format favorites URL's to new format
-function new_favorites_url($q) {
+function new_favorites_url($q)
+{
     $p = array();
     if (array_key_exists('books', $q)) {
         $v = $q['books'];
@@ -15,11 +14,23 @@ function new_favorites_url($q) {
     } else {
         return favorites_url();
     }
-    foreach (array('bgcolor'=>'pageColor', 'fgcolor'=>'textColor') as $old=>$new) {
+    foreach (
+        array('bgcolor' => 'pageColor', 'fgcolor' => 'textColor')
+        as $old => $new
+    ) {
         if (array_key_exists($old, $q)) {
             $cname = $q[$old];
-            $c = array('black'=>'000', 'blue'=>'00f', 'green'=>'0f0', 'cyan'=>'0ff', 'red'=>'f00', 'magenta'=>'f0f', 'yellow'=>'ff0', 'white'=>'fff');
-            $d = array('pageColor'=>'fff', 'textColor'=>'000');
+            $c = array(
+                'black' => '000',
+                'blue' => '00f',
+                'green' => '0f0',
+                'cyan' => '0ff',
+                'red' => 'f00',
+                'magenta' => 'f0f',
+                'yellow' => 'ff0',
+                'white' => 'fff'
+            );
+            $d = array('pageColor' => 'fff', 'textColor' => '000');
             $v = $c[$cname] ?: $d[$new];
             $p[] = "$new=$v";
         }
@@ -32,24 +43,30 @@ function new_favorites_url($q) {
     $result = '/favorites/?' . implode('&', $p);
     return $result;
 }
-?>
-<?php
 // redirect on old style favorites URLs to convert them to new style
-if (array_key_exists('books', $_GET)) { // old format URL, convert it before redirect
+if (array_key_exists('books', $_GET)) {
+    // old format URL, convert it before redirect
     $loc = new_favorites_url($_GET);
     header('Location: ' . $loc);
     die();
 }
 
 // redirect on an empty URL so the page is bookmarkable
-if (! array_key_exists('favorites', $_GET) && ! array_key_exists('collection', $_GET) && THR('favorites')) {
+if (
+    !array_key_exists('favorites', $_GET) &&
+    !array_key_exists('collection', $_GET) &&
+    THR('favorites')
+) {
     $loc = favorites_url();
     header('Location: ' . $loc);
     die();
 }
 
 // redirect on A or R URLs so the resulting URL reflects the state
-if (array_key_exists('favorites', $_GET) && preg_match('/^[AR]/', $_GET['favorites'])) {
+if (
+    array_key_exists('favorites', $_GET) &&
+    preg_match('/^[AR]/', $_GET['favorites'])
+) {
     $loc = favorites_url();
     header('Location: ' . $loc);
     die();
@@ -60,7 +77,12 @@ $where = array();
 //$where[] = "p.post_status = 'publish'";
 $collection = THR('collection');
 if ($collection) {
-    $favorites = $wpdb->get_var($wpdb->prepare("SELECT booklist FROM $collections_table WHERE slug = %s", $collection));
+    $favorites = $wpdb->get_var(
+        $wpdb->prepare(
+            "SELECT booklist FROM $collections_table WHERE slug = %s",
+            $collection
+        )
+    );
     setTHR('favorites', $favorites);
     thr_setcookie(1); // make sure the client sees the updated favorties
 } else {
@@ -81,7 +103,9 @@ $page = THR('fpage');
 $offset = ($page - 1) * $count;
 
 $sql = "
-SELECT p.*
+SELECT p.*, exists (select 1 from wpreader_shared 
+                      where ID = p.ID and 
+                      status = 'published' $shared_where) as comments
     FROM wpreader_posts p
     $where
     LIMIT $offset,$cp1";
@@ -90,13 +114,18 @@ $posts = $wpdb->get_results($sql);
 $nrows = $wpdb->num_rows;
 
 $result = posts_to_find_results($posts, $nrows, $count);
+foreach ($result['books'] as &$book) {
+    $book['link'] = '/shared/read/' . $book['slug'];
+}
 
-if (0) { // force an error for testing
+if (0) {
+    // force an error for testing
     header("HTTP/1.0 500 Internal Error");
     die();
 }
-if (0) { // delay for testing
-        sleep(3);
+if (0) {
+    // delay for testing
+    sleep(3);
 }
 if ($json) {
     $output = json_encode($result);
@@ -106,26 +135,24 @@ if ($json) {
     die();
 }
 setTHR('findAnotherLink', '/favorites/');
-?>
-<?php
-    thr_header('favorites-page', array('settings'=>true, 'chooseFavorites'=>true));
-?>
-<!-- favorites.php -->
-<?php
+thr_header('favorites-page', array(
+    'settings' => true,
+    'chooseFavorites' => true
+));
 $view = array();
 $view['searchForm'] = '';
 // edit the data to create the view for the template
-foreach( $result['books'] as &$book ) {
+foreach ($result['books'] as &$book) {
     $c = &$book['cover'];
     setImageSizes($c);
 }
 $result['favorites'] = true;
 $view['bookList'] = template_render('bookList', $result);
 if ($page > 1) {
-    $view['backLink'] = favorites_url($page-1);
+    $view['backLink'] = favorites_url($page - 1);
 }
 if ($result['more']) {
-    $view['nextLink'] = favorites_url($page+1);
+    $view['nextLink'] = favorites_url($page + 1);
 }
 echo template_render('find', $view);
 
